41ede8f8e927daad32bc3023d32050469b75c4bb
ScottCoffin <56366258+ScottCoffin@users.noreply.github.com>
2025-11-14 05:45:08 +0000

    Deploying to gh-pages from @ OEHHA-NTES/OEHHA-NTES.github.io@b77efbd506e0d7cc6768b84f7a108a0ff1fcd9d9 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -31,7 +31,7 @@ ninja.data = [{
             window.location.href = "/projects/";
           },
         },{id: "nav-applications",
-          title: "Applications",
+          title: "applications",
           description: "R/Shiny tools for toxicology, PFAS pharmacokinetics, and lead exposure modeling.",
           section: "Navigation",
           handler: () => {
